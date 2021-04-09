1e2bac9e63d3b6159af5165c21d30b460346b8e1
Andrew Arrow <oneone@gmail.com>
2021-04-09 13:32:16 -0700

    pattern

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -22,7 +23,28 @@ func main() {
 		fullDate, _ := time.Parse("01/02/2006 15:04", fullMoon)
 
 		fmt.Println(newDate, fullDate)
-		fmt.Println(newDate.Unix(), fullDate.Unix(), fullDate.Unix()-newDate.Unix())
+		delta := fullDate.Unix() - newDate.Unix()
+		deltaString := fmt.Sprintf("%d", delta)
+		fmt.Println(newDate.Unix(), fullDate.Unix(), delta,
+			AsciiByteToBase9(deltaString))
 
 	}
+}
+
+func AsciiByteToBase9(a string) byte {
+
+	sum := byte(0)
+	for i := range a {
+
+		word := a[i : i+1]
+		t, _ := strconv.Atoi(word)
+
+		sum += byte(t)
+	}
+	strSum := fmt.Sprintf("%d", sum)
+	if len(strSum) > 1 {
+		return AsciiByteToBase9(strSum)
+	}
+	return sum
+
 }
