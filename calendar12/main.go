08f61e1d4ef7dc8a02fcd0bba15c8cc5e1e1e243
Andrew Arrow <oneone@gmail.com>
2021-04-18 22:09:49 -0700

    logic

--- calendar12/main.go
+++ calendar12/main.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 func main() {
+	b, _ := ioutil.ReadFile("/Users/andrewarrow/data/1900")
+	s := string(b)
+	on := false
+	for _, line := range strings.Split(s, "\n") {
+		if strings.Contains(line, "moon_phase_name") {
+			tokens := strings.Split(line, "<")
+			for _, t := range tokens {
+				if on {
+					more := strings.Split(t, ">")
+					if len(more) > 1 {
+						fmt.Println(more[1])
+					}
+				}
+				if t == "span>New Moon" || t == "span>Full Moon" {
+					on = true
+				}
+				if t == "/tr>" {
+					on = false
+				}
+			}
+		}
+	}
+}
+
+func main2() {
 	year := 1901
 	for {
 		fmt.Printf("wget https://www.calendar-12.com/moon_phases/%d\n", year)
