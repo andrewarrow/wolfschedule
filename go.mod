c61885dccbf60efee7668847791514889b29b30a
Andrew Arrow <oneone@gmail.com>
2021-04-19 07:54:44 -0700

    counting

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module wolfschedule
 
 go 1.16
 
-require golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb
+require (
+	github.com/otiai10/primes v0.0.0-20180210170552-f6d2a1ba97c4
+	golang.org/x/image v0.0.0-20210220032944-ac19c3e999fb
+)
--- pattern.go
+++ pattern.go
@@ -6,9 +6,11 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/otiai10/primes"
 )
 
-func ParseForPattern() {
+func ParseForPattern2() {
 
 	b, _ := ioutil.ReadFile("1970_2100.csv")
 	s := string(b)
@@ -29,6 +31,34 @@ func ParseForPattern() {
 		}
 
 		prevYear = year
+	}
+	fmt.Println("")
+	fmt.Println("")
+}
+func ParseForPattern() {
+
+	b, _ := ioutil.ReadFile("1970_2100.csv")
+	s := string(b)
+	prevTime := int64(0)
+	for _, line := range strings.Split(s, "\n") {
+		tokens := strings.Split(line, ",")
+		if len(tokens) < 3 {
+			break
+		}
+		ts, _ := strconv.ParseInt(tokens[1], 10, 64)
+		if prevTime > 0 {
+			delta := ts - prevTime
+			//maybe := 84560
+			//maybe := 90600
+			//fmt.Println(delta, float64(delta)/86400, float64(delta)/float64(maybe))
+			deltaString := fmt.Sprintf("%d", delta)
+			factors := primes.Factorize(delta).All()
+			digit := AsciiByteToBase9(deltaString)
+			deltaString = fmt.Sprintf("%d", factors[len(factors)-1])
+			fmt.Println(delta, digit, factors[len(factors)-1], AsciiByteToBase9(deltaString))
+		}
+
+		prevTime = ts
 	}
 	fmt.Println("")
 	fmt.Println("")
