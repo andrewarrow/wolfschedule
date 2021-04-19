7054f090bad14c9c2ae17b36a93a56a7619ae286
Andrew Arrow <oneone@gmail.com>
2021-04-18 22:28:23 -0700

    newline

--- calendar12/main.go
+++ calendar12/main.go
@@ -10,6 +10,7 @@ func main() {
 	b, _ := ioutil.ReadFile("/Users/andrewarrow/data/1900")
 	s := string(b)
 	on := false
+	buff := []string{}
 	for _, line := range strings.Split(s, "\n") {
 		if strings.Contains(line, "moon_phase_name") {
 			tokens := strings.Split(line, "<")
@@ -17,7 +18,9 @@ func main() {
 				if on {
 					more := strings.Split(t, ">")
 					if len(more) > 1 {
-						fmt.Println(more[1])
+						if more[1] != "" {
+							buff = append(buff, more[1])
+						}
 					}
 				}
 				if t == "span>New Moon" || t == "span>Full Moon" {
@@ -27,6 +30,26 @@ func main() {
 					on = false
 				}
 			}
+		}
+	}
+
+	buff2 := []string{}
+	for i, line := range buff {
+		if i%2 == 0 {
+			item := line[0:(len(line)-3)] + "1900 "
+			buff2 = append(buff2, item)
+		} else {
+			buff2 = append(buff2, line+"")
+		}
+	}
+	buff3 := []string{}
+	for i := 0; i < len(buff2)-1; i += 2 {
+		item := fmt.Sprintf("%s%s", buff2[i], buff2[i+1])
+		buff3 = append(buff3, item)
+	}
+	for i, line := range buff3 {
+		if i%2 != 0 {
+			fmt.Println(line)
 		}
 	}
 }
