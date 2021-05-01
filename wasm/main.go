3649e6090f89783168fac582b2ad5b3c29fc91df
Andrew Arrow <oneone@gmail.com>
2021-05-01 09:23:05 -0700

    wasm

--- wasm/main.go
+++ wasm/main.go
@@ -4,12 +4,27 @@ import (
 	"embed"
 	"fmt"
 	"io/fs"
+	"syscall/js"
 )
 
 //go:embed *.csv
 var embededFiles embed.FS
 
+func jsonWrapper() js.Func {
+	jsonFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		if len(args) != 1 {
+			return "Invalid no of arguments passed"
+		}
+		inputJSON := args[0].String()
+		fmt.Printf("input %s\n", inputJSON)
+		return "pretty"
+	})
+	return jsonFunc
+}
+
 func main() {
+	js.Global().Set("wolfData", jsonWrapper())
+
 	fsys, err := fs.Sub(embededFiles, ".")
 	if err != nil {
 		panic(err)
