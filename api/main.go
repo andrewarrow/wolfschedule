137f7c9864a02fd1bd66a226c477a032d8dfd563
Andrew Arrow <oneone@gmail.com>
2021-05-15 11:00:38 -0700

    space

--- api/main.go
+++ api/main.go
@@ -20,26 +20,26 @@ func main() {
 			Limit: 100,
 		})
 
-	fmt.Printf("%s %s %s %s %s %s %s %s\n",
+	fmt.Printf("%s %s %s %s%s%s %s %s\n",
 		display.LeftAligned("", 10),
 		display.LeftAligned("", 8),
 		display.LeftAligned("Pairs", 10),
 		display.LeftAligned("Max", 10),
-		display.LeftAligned("Cir", 10),
+		display.LeftAligned("Cir", 12),
 		display.LeftAligned("Total", 12),
 		display.LeftAligned("USD", 10),
 		display.LeftAligned("Cap", 10))
 	for _, c := range listings {
 		usd := c.Quote["USD"].Price
 		mcap := (c.CirculatingSupply / 1000000000.0) * usd
-		fmt.Printf("%s %s %s %s %s %s %s %s\n",
+		fmt.Printf("%s %s %s %s%s%s %s %s\n",
 			//display.LeftAligned(c.Name, 10),
 			display.LeftAligned(c.DateAdded, 10),
 			display.LeftAligned(c.Symbol, 8),
 			display.LeftAligned(c.NumMarketPairs, 10),
 			// CMCRank
 			display.LeftAligned(fmt.Sprintf("%0.2f", c.MaxSupply/1000000000.0), 10),
-			display.LeftAligned(fmt.Sprintf("%0.2f", c.CirculatingSupply/1000000000.0), 10),
+			display.LeftAligned(fmt.Sprintf("%0.2f", c.CirculatingSupply/1000000000.0), 12),
 			display.LeftAligned(fmt.Sprintf("%0.2f", c.TotalSupply/1000000000.0), 12),
 			display.LeftAligned(fmt.Sprintf("%0.2f", usd), 10),
 			display.LeftAligned(fmt.Sprintf("%0.2f", mcap), 10))
