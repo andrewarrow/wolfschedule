ddada9d34895104f851baae5746150b21c907f31
Andrew Arrow <oneone@gmail.com>
2021-05-15 10:16:12 -0700

    free

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,9 @@
 module api
 
 go 1.16
+
+require (
+	github.com/anaskhan96/soup v1.2.4 // indirect
+	github.com/coincircle/go-coinmarketcap v0.0.0-20180522222104-86a427de333f // indirect
+	github.com/miguelmota/go-coinmarketcap v0.1.7
+)
--- api/main.go
+++ api/main.go
@@ -3,9 +3,24 @@ package main
 import (
 	"fmt"
 	"os"
+
+	//cmc "github.com/coincircle/go-coinmarketcap"
+	cmc "github.com/miguelmota/go-coinmarketcap/pro/v1"
 )
 
 func main() {
 	api := os.Getenv("CMC")
 	fmt.Println("key", api)
+
+	client := cmc.NewClient(&cmc.Config{ProAPIKey: api})
+
+	listings, _ := client.Cryptocurrency.
+		LatestListings(&cmc.ListingOptions{
+			Limit: 10,
+		})
+
+	for _, listing := range listings {
+		fmt.Println(listing.Name)
+		fmt.Println(listing.Quote["USD"].Price)
+	}
 }
