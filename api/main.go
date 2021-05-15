d8f3e122106f0e30446b2e8e750e9483e03a5a55
Andrew Arrow <oneone@gmail.com>
2021-05-15 10:34:52 -0700

    api

--- api/main.go
+++ api/main.go
@@ -20,19 +20,22 @@ func main() {
 			Limit: 10,
 		})
 
+	fmt.Printf("%s %s %s %s %s %s\n",
+		display.LeftAligned("", 10),
+		display.LeftAligned("", 10),
+		display.LeftAligned("Pairs", 10),
+		display.LeftAligned("Max", 10),
+		display.LeftAligned("Circulating", 15),
+		display.LeftAligned("Total", 10))
 	for _, c := range listings {
-		fmt.Printf("%s %s %s\n",
+		fmt.Printf("%s %s %s %s %s %s\n",
 			//display.LeftAligned(c.Name, 10),
+			display.LeftAligned(c.DateAdded, 10),
 			display.LeftAligned(c.Symbol, 10),
-			// CirculatingSupply
-			// DateAdded
-			// TotalSupply
-			// MaxSupply
-			// Symbol
-			// DateAdded
-			// NumMarketPairs
+			display.LeftAligned(c.NumMarketPairs, 10),
 			// CMCRank
-			display.LeftAligned(fmt.Sprintf("%0.2f", c.CirculatingSupply/1000000.0), 10),
+			display.LeftAligned(fmt.Sprintf("%0.2f", c.MaxSupply/1000000.0), 10),
+			display.LeftAligned(fmt.Sprintf("%0.2f", c.CirculatingSupply/1000000.0), 15),
 			display.LeftAligned(fmt.Sprintf("%0.2f", c.TotalSupply/1000000.0), 10))
 	}
 }
