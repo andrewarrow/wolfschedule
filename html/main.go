81814c093f7756716965072e3424bcbd98b63edb
Andrew Arrow <oneone@gmail.com>
2022-04-08 13:35:21 -0700

    year

--- html/main.go
+++ html/main.go
@@ -10,10 +10,19 @@ func main() {
 	timeZone, _ := time.LoadLocation("America/Phoenix")
 	eventDate := time.Date(year, time.Month(1), 1, 0, 0, 0, 0, timeZone)
 	month := eventDate.Month()
+
 	for {
-		fmt.Println(eventDate, eventDate.Weekday())
-		eventDate = eventDate.AddDate(0, 0, 1)
-		if eventDate.Month() != month {
+		for {
+			fmt.Println(eventDate, eventDate.Weekday())
+			eventDate = eventDate.AddDate(0, 0, 1)
+			if eventDate.Month() != month {
+				month = eventDate.Month()
+				fmt.Println("")
+				break
+			}
+		}
+
+		if eventDate.Year() != year {
 			break
 		}
 	}
