380ba3a486aa6ad8cd915361a8a9812f6e1791b0
Andrew Arrow <oneone@gmail.com>
2022-05-30 21:54:54 -0700

    template

--- server/server/welcome.go
+++ server/server/welcome.go
@@ -3,12 +3,17 @@ package server
 import (
 	"net/http"
 
+	"html/template"
+
 	"github.com/gin-gonic/gin"
 )
 
 func WelcomeIndex(c *gin.Context) {
 
+	body := template.HTML("<p>wefwefwef</p>")
+
 	c.HTML(http.StatusOK, "index.tmpl", gin.H{
 		"flash": "",
+		"body":  body,
 	})
 }
