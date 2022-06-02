41ea208e78e3c5e0eea38238e1937d36254e1f0a
Andrew Arrow <oneone@gmail.com>
2022-06-02 11:14:41 -0700

    events

--- moon/events.go
+++ moon/events.go
@@ -1,17 +1,45 @@
 package moon
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
-func FindNextEvent(t int64) {
-	for k, v := range times {
-		fmt.Println(k, v)
-	}
+type Event struct {
+	Timestamp int64
+	FullMoon  bool
 }
 
-var times map[int64]bool = map[int64]bool{
-	1653910320: false,
-	1655207520: true,
-	1656471180: false,
-	1657737480: true,
-	1659030900: false,
+func NewEvent(ts int64, b bool) *Event {
+	e := Event{}
+	e.Timestamp = ts
+	e.FullMoon = b
+	return &e
+}
+
+func (e *Event) String() string {
+	t := time.Unix(e.Timestamp, 0)
+	tstr := fmt.Sprintf("%v", t)
+	if e.FullMoon {
+		return fmt.Sprintf("FULL %s", tstr)
+	}
+	return fmt.Sprintf("NEW %s", tstr)
+}
+
+func FindNextEvent(t int64) *Event {
+	for _, k := range timeList {
+		if k.Timestamp > t {
+			return &k
+		}
+	}
+
+	return nil
+}
+
+var timeList = []Event{
+	Event{1653910320, false},
+	Event{1655207520, true},
+	Event{1656471180, false},
+	Event{1657737480, true},
+	Event{1659030900, false},
 }
--- moon/events_test.go
+++ moon/events_test.go
@@ -1,11 +1,13 @@
 package moon
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestFindNextEvent(t *testing.T) {
 	ts := time.Now().Unix()
-	FindNextEvent(ts)
+	e := FindNextEvent(ts)
+	fmt.Println(e.String())
 }
