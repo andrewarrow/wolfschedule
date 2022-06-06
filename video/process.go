46fa33505ce2e1b6049a8ecfd173ddf5eac8f5eb
Andrew Arrow <oneone@gmail.com>
2022-06-06 12:50:44 -0700

    done

--- video/process.go
+++ video/process.go
@@ -18,19 +18,35 @@ func ProcessDirectory(dir string) {
 			continue
 		}
 		parts := strings.Split(tokens[1], ".")
-		outputDir := fmt.Sprintf("%s/DONE_%s", dir, parts[0])
-		os.Mkdir(outputDir, 0755)
-		outputFilename := fmt.Sprintf("DONE_%s/source.mov", parts[0])
-		details := "scale=w=880:h=720:force_original_aspect_ratio=1,pad=880:720:(ow-iw):(oh-ih)"
-		cmd := exec.Command("/usr/local/bin/ffmpeg", "-i", name, "-vf", details, outputFilename)
-		cmd.Dir = dir
-		output, e := cmd.CombinedOutput()
-		if e != nil {
-			fmt.Println(name, e)
-			return
-		}
-		fmt.Println(name, string(output))
+		ScaleOrig(dir, name, parts[0])
+		ExtractFrames(dir, parts[0])
 	}
+}
+
+func ScaleOrig(dir, name, part string) {
+	outputDir := fmt.Sprintf("%s/DONE_%s", dir, part)
+	os.Mkdir(outputDir, 0755)
+	outputFilename := fmt.Sprintf("DONE_%s/source.mov", part)
+	details := "scale=w=880:h=720:force_original_aspect_ratio=1,pad=880:720:(ow-iw):(oh-ih)"
+	cmd := exec.Command("/usr/local/bin/ffmpeg", "-i", name, "-vf", details, outputFilename)
+	cmd.Dir = dir
+	output, e := cmd.CombinedOutput()
+	if e != nil {
+		fmt.Println(name, e)
+		return
+	}
+	fmt.Println(name, string(output))
+}
+
+func ExtractFrames(dir, part string) {
+	cmd := exec.Command("/usr/local/bin/ffmpeg", "-i", "source.mov", "-vf", "fps=29.97", "img%07d.png")
+	cmd.Dir = fmt.Sprintf("%s/DONE_%s", dir, part)
+	output, e := cmd.CombinedOutput()
+	if e != nil {
+		fmt.Println(part, e)
+		return
+	}
+	fmt.Println(part, string(output))
 }
 
 //  ffmpeg -i IMG_2367.MOV -vf "scale=w=880:h=720:force_original_aspect_ratio=1,pad=880:720:(ow-iw):(oh-ih)" output.mov
