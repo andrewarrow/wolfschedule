66baaf6ca06c0a4f7d7e7b26cc9fe41606ca9c34
Andrew Arrow <oneone@gmail.com>
2022-06-06 10:51:42 -0700

    notes

--- video/process.go
+++ video/process.go
@@ -32,3 +32,4 @@ func ProcessDirectory(dir string) {
 //  ffmpeg -i IMG_2367.MOV -vf "scale=w=880:h=720:force_original_aspect_ratio=1,pad=880:720:(ow-iw):(oh-ih)" output.mov
 // ffmpeg -i DONE_2367.MOV -vf fps=29.97 img%05d.png
 //  ffmpeg -framerate 29.97 -pattern_type glob -i '*.png' -c:v libx264 -pix_fmt yuv420p out.mp4
+// ffmpeg -i out.mp4 -i DONE_2367.mp3 -c copy -map 0:v:0 -map 1:a:0 sound.mov
