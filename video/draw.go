5e92546d1a574b828e36534df1730b2867950092
Andrew Arrow <oneone@gmail.com>
2022-06-12 08:00:46 -0700

    time

--- video/draw.go
+++ video/draw.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/andrewarrow/wolfschedule/moon"
 	"github.com/fogleman/gg"
 )
 
@@ -29,8 +30,11 @@ func DrawOneFrame(modtime time.Time) {
 
 	i := 1
 	t := modtime.Add(time.Second * time.Duration(i))
-	dc.DrawStringAnchored("Next FULL MOON in", 310, 430, 0.5, 0.5)
-	dc.DrawStringAnchored("1 day(s), 21 hour(s), 1 min(s)", 310, 460, 0.5, 0.5)
+	event := moon.FindNextEvent(t.Unix())
+	str := fmt.Sprintf("Next %s MOON in", event.NewOrFull())
+	dc.DrawStringAnchored(str, 310, 430, 0.5, 0.5)
+	deltaString := moon.EventDelta(event.Timestamp - t.Unix())
+	dc.DrawStringAnchored(deltaString, 310, 460, 0.5, 0.5)
 	dc.DrawStringAnchored(t.Format(time.RFC850), 310, 520, 0.5, 0.5)
 
 	dc.SavePNG("test.png")
